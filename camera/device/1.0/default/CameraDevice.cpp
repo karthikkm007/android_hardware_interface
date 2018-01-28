3e519f3407c05a19bfb9d400700bd4e896c6b882
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-28 11:34:01 -0800

    camera: Only link and use vendor.qti.hardware.camera.device if specified
    Only use if TARGET_USES_QTI_CAMERA_DEVICE is set
    
    Change-Id: I74b178c2f685a2ffda3e91eb7ff6d05664eebaf6
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- camera/device/1.0/default/CameraDevice.cpp
+++ camera/device/1.0/default/CameraDevice.cpp
@@ -447,6 +447,7 @@ void CameraDevice::sDataCb(int32_t msg_type, const camera_memory_t *data, unsign
              index, mem->mNumBufs);
         return;
     }
+#ifdef QTI_CAMERA_DEVICE
     if(object->mQDeviceCallback != nullptr) {
          vendor::qti::hardware::camera::device::V1_0::QCameraFrameMetadata hidlMetadata;
          if (metadata) {
@@ -483,8 +484,8 @@ void CameraDevice::sDataCb(int32_t msg_type, const camera_memory_t *data, unsign
          CameraHeapMemory* mem = static_cast<CameraHeapMemory *>(data->handle);
          object->mQDeviceCallback->QDataCallback(
                  (DataCallbackMsg) msg_type, mem->handle.mId, index, hidlMetadata);
- 
     } else {
+#endif
        if (object->mDeviceCallback != nullptr) {
            CameraFrameMetadata hidlMetadata;
            if (metadata) {
@@ -509,7 +510,9 @@ void CameraDevice::sDataCb(int32_t msg_type, const camera_memory_t *data, unsign
            CameraHeapMemory* mem = static_cast<CameraHeapMemory *>(data->handle);
            object->mDeviceCallback->dataCallback(
                    (DataCallbackMsg) msg_type, mem->handle.mId, index, hidlMetadata);
+#ifdef QTI_CAMERA_DEVICE
        }
+#endif
     }
 }
 
@@ -730,10 +733,12 @@ Return<Status> CameraDevice::open(const sp<ICameraDeviceCallback>& callback) {
 
     initHalPreviewWindow();
     mDeviceCallback = callback;
+#ifdef QTI_CAMERA_DEVICE
     mQDeviceCallback = vendor::qti::hardware::camera::device::V1_0::IQCameraDeviceCallback::castFrom(callback);
     if(mQDeviceCallback == nullptr) {
         ALOGI("could not cast ICameraDeviceCallback to IQCameraDeviceCallback");
     }
+#endif
 
 #ifdef TARGET_USES_NON_TREBLE_CAMERA
     sCameraDevice = this;
